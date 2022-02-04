e04c650c74805e2898188c4c0b621ec713d2d61b
PatrickKumer <patrick.kumer@gmail.com>
2022-02-04 13:23:31 +0100

    Did exercise 21 and 22

---
+++ exercise-D10.js
@@ -0,0 +1,24 @@
+const printTitle = function(nameOfTitle){
+    console.log(`\n${nameOfTitle}`)
+}
+
+
+
+
+printTitle("21")
+
+let x = "John"
+let y = "Doe"
+let z = "<>"
+
+console.log(x + z + y)
+
+printTitle("22")
+
+let personal = {
+    name: "Mark",
+    lastName: "Robertson",
+    email: "mark.robertson123@gmail.com"
+}
+
+console.log(personal)
